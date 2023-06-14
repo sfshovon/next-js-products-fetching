59f8b436509e0d537d0bd04749dadaf59247b1eb
sfshovon <shahriar.shovon@northsouth.edu>
2023-06-14 20:03:41 +0600

    Added Quick View Button With Transition

--- src/app/Components/Product.jsx
+++ src/app/Components/Product.jsx
@@ -4,7 +4,7 @@ import { AiFillStar } from 'react-icons/ai';
 const Product = ({ product }) => {
   const totalPrice = parseInt(product?.variations[0]?.price?.mrp);
   const discountedPrice = parseInt(product?.variations[0]?.price?.discountedPrice);
-  const discountPercentage = parseInt(( (totalPrice - discountedPrice) / totalPrice) * 100);
+  const discountPercentage = parseInt(((totalPrice - discountedPrice) / totalPrice) * 100);
   const showRatings = (rating) => {
     const stars = [];
     for (let i = 0; i < rating; i++) {
@@ -19,32 +19,44 @@ const Product = ({ product }) => {
 
   return (
     <div className="card h-96">
-      <figure>
-       <Image className="w-full" src={product?.productImage} width={200} height={300} alt="Product" />
+      <figure className="relative group">
+        <Image
+          className="w-full transition-transform duration-1200 ease-in transform-gpu hover:scale-110"
+          src={product?.productImage}
+          width={200}
+          height={300}
+          alt="Product"
+        />
+        <div className="absolute inset-x-0 bottom-0 opacity-0 transition-opacity group-hover:opacity-100">
+          <button className="from-bottom w-full flex justify-center items-center bg-gradient-to-r from-green-400 to-cyan-700 text-lg text-white font-bold py-2 px-4">
+            Quick View
+          </button>
+        </div>
       </figure>
-      { 
-        discountPercentage > 0 &&
-          <h3 className="bg-green-600 text-white py-1 px-4 absolute top-2 left-2 text-sm">
-            -{discountPercentage}%
-          </h3>
-      }
+      {discountPercentage > 0 && (
+        <h3 className="bg-green-600 text-white py-1 px-4 absolute top-2 left-2 text-sm">
+          -{discountPercentage}%
+        </h3>
+      )}
       <div className="card-body">
         <div className="group">
-          <h2 className="text-center text-lg text-zinc-500 font-semibold truncate shadow-sm">{product?.name}</h2>
+          <h2 className="text-center text-lg text-zinc-500 font-semibold truncate shadow-sm">
+            {product?.name}
+          </h2>
           <h3 className="absolute top-40 scale-0 rounded bg-cyan-800 p-2 text-sm text-white group-hover:scale-100">
             {product?.name}
           </h3>
         </div>
-        { 
-          discountPercentage > 0 &&
-            <div className="flex justify-center items-center gap-2">
-              <h3 className="text-sm text-dark">MRP <span className="font-semibold line-through">{totalPrice}</span></h3>
-              { 
-                discountPercentage > 0 &&
-                <h3 className="text-sm text-slate-500 font-semibold">-{discountPercentage}%</h3>
-              }
-            </div>
-        } 
+        {discountPercentage > 0 && (
+          <div className="flex justify-center items-center gap-2">
+            <h3 className="text-sm text-dark">
+              MRP <span className="font-semibold line-through">{totalPrice}</span>
+            </h3>
+            {discountPercentage > 0 && (
+              <h3 className="text-sm text-slate-500 font-semibold">-{discountPercentage}%</h3>
+            )}
+          </div>
+        )}
         <h3 className="text-lg text-dark text-center font-bold">BDT {discountedPrice}</h3>
         <h3 className="text-sm text-center">{showRatings(parseInt(product?.rating))}</h3>
       </div>
